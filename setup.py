ab1e9ef37a9b3a7373c958b9a3926e166bdea183
Mohamed Elhayany <40873107+Melhaya@users.noreply.github.com>
2021-12-20 23:08:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name="webcatjupyterplugin",
     version='0.1.0',
-    url="https://github.com/CSSPLICE/webcatjupyterplugin",
+    url="https://github.com/Melhaya/webcatjupyterplugin",
     author="Hamza Manzoor",
     description="Jupyter Notebook Plugin for submitting files to Web-CAT",
     packages=setuptools.find_packages(),
